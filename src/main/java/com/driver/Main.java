b59c9ebf6193efca5eccc9bdb39c4fd0813ec133
Deepanshigreza <deepanshigreza@gmail.com>
2022-12-12 00:29:42 +0530

    updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
-public class Main {
-  
+abstract class A {
+    public static String meth(){
+        return "hello";
+    }
+
+
 }
+class B extends A{
+
+    public static String meth(){
+        return "Method is overridden in Extended class B";
+    }
+
+}
+public class Main {
+    public static void main(String[] args){
+        A obj1=new B();
+        B obj2=new B();
+  System.out.println(obj2.meth());
+}}
